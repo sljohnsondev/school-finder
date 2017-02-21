e8dd5b9d3b43ffbaee50186c36e1e2242d5a8587
sljohnson32 <sljohnson32@gmail.com>
2017-02-21 16:34:39 -0700

    more setup

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../Header';
+import Map from '../Map';
 import './app-style.css'
 
 export default class App extends Component {
@@ -12,8 +13,11 @@ export default class App extends Component {
     return (
       <div>
         <Header />
+        <Map />
         {this.props.children}
       </div>
     )
   }
 }
+//
+// AIzaSyB8JYY9Fxzlc0pjxOxv-i9f1QtLOe0lY9o
---
+++ src/components/Map/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Map extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+// <iframe
+//   width="100vw"
+//   height="100vh"
+//   frameborder="0" style="border:0"
+//   src="https://www.google.com/maps/embed/v1/search?key=AIzaSyB8JYY9Fxzlc0pjxOxv-i9f1QtLOe0lY9o&center=39.730742,-104.960202" allowfullscreen>
+// </iframe>
