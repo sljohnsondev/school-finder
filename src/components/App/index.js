e23604deae348bce9ad3c676d9145ada0d01a8a0
sljohnson32 <sljohnson32@gmail.com>
2017-02-22 10:28:00 -0700

    Initial map component setup

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../Header';
-import GoogleMap from '../GoogleMap';
+import Map from '../Map';
 import './app-style.css'
 
 export default class App extends Component {
@@ -11,9 +11,9 @@ export default class App extends Component {
 
   render() {
     return (
-      <div>
+      <div style={{width: 700, height: 800, background: 'peru'}}>
         <Header />
-        <GoogleMap />
+        <Map />
         {this.props.children}
       </div>
     )
