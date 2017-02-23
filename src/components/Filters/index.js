69739bb7e8fd428604fe7fb56949d115c5a9d452
sljohnson32 <sljohnson32@gmail.com>
2017-02-23 14:16:52 -0700

    add signin and filter styling

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -6,9 +6,9 @@ export default class Filters extends Component {
 
   render() {
     return (
-      <div>
+      <div className='filter-container'>
         <h2 className='filter-header'>Search Filters</h2>
-        <div className='filter-container'>
+        <div className='filter-fields'>
           Filter fields will go here
         </div>
         <button className='search-btn'>Submit</button>
