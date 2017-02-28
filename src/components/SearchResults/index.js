5b0bd73c42ca84e631ec34d76d6c8565ac90c206
sljohnson32 <sljohnson32@gmail.com>
2017-02-27 17:02:19 -0700

    build out results a bit more

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import './searchresults-style.css';
 
 export default class SearchResults extends Component {
 
-  render() {
+  constructor() {
+    super()
+    this.state ={
+      selected: false
+    }
+  }
 
+  toggleSchoolSelect() {
+    this.setState({ selected: !this.state.selected })
+  }
+
+  render() {
+    let { Name, Address, Phone, WebUrl } = this.props.schoolData
     return (
-      <div className='school-container'>
-        <h4 className='filter-fields'>{this.props.schoolData.Name}</h4>
-        <p>Siiiick info about school...</p>
+      <div className={this.state.selected ? 'school-container selected' : 'school-container'} onClick={() => this.toggleSchoolSelect()}>
+        <h3 className='filter-fields'>{Name}</h3>
+        <p>{Address}</p>
+        <a href={WebUrl} className='school-url' target='_blank'>School Website</a>
+        <section className='commute-info'>
+          <p>Commuted Time: TBD</p>
+          <p>Commuted Distance: TBD</p>
+        </section>
       </div>
     )
   }
