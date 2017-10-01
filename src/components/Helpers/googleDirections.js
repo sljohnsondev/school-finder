798b041fbd4ace24ad47f58ca9b5d7aad3dbcb5f
sljohnson32 <sljohnson32@gmail.com>
2017-10-01 14:52:36 -0600

    Resolve departure time issue for transit

--- src/components/Helpers/googleDirections.js
+++ src/components/Helpers/googleDirections.js
@@ -1,12 +1,15 @@
+import moment from 'moment'
+
 const googleDirections = (homeAddress, schoolAddress, transitMode, callback) => {
   let origin = new window.google.maps.LatLng(homeAddress.Location.Lat, homeAddress.Location.Lng );
   let destinations = new window.google.maps.LatLng(schoolAddress.Location.Lat, schoolAddress.Location.Lng );
+  let departTime = new Date(moment().startOf('isoWeek').add(1, 'week').add(7.5, 'hours').valueOf())
 
   let request = {
     origin: origin,
     destination: destinations,
     travelMode: transitMode,
-    transitOptions: {departureTime: new Date(1505309400 *1000) },
+    transitOptions: {departureTime: departTime },
     unitSystem: window.google.maps.UnitSystem.IMPERIAL
   }
   let service = new window.google.maps.DirectionsService();
