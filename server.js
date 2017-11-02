900e12c38ac15634139cd6c09ecd84ca1611caf0
Dan Alvarez <danalvarez5280@gmail.com>
2017-11-01 23:35:18 -0600

    add get, posts, deletes in server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/v1/users', (request, response) => {
-  database('school_finder_users').select()
+  database('users').select()
     .then((users) => {
       response.status(200).json(users);
     })
@@ -25,3 +25,87 @@ app.get('/api/v1/users', (request, response) => {
       response.status(500).json({ error });
     });
 });
+
+app.get('/api/v1/favorites', (request, response) => {
+  database('favorites').select()
+    .then((favorites) => {
+      response.status(200).json(favorites);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
+app.post('/api/v1/users', (request, response) => {
+  const user = request.body;
+
+  for (let requiredParameter of ['username', 'email', 'street_address', 'oath_id']) {
+    if (!user[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { name: <String>, email: <String>, street_address: <String>, oath_id: <String>}. You're missing a '${requiredParameter}' property.` });
+    }
+  }
+
+  database('users').insert(user, 'id')
+  .then(user => {
+    response.status(201).json({ id: user[0] });
+  })
+  .catch(error => {
+    response.status(500).json({ error });
+  });
+});
+
+app.post('/api/v1/favorites', (request, response) => {
+  const user = request.body;
+
+  for (let requiredParameter of ['school_id', 'school_name', 'school_code', 'user_id']) {
+    if (!user[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { school_id: <String>, school_name: <String>, school_code: <String>, user_id: <String>}. You're missing a '${requiredParameter}' property.` });
+    }
+  }
+
+  database('favorites').insert(user, 'id')
+  .then(user => {
+    response.status(201).json({ id: user[0] });
+  })
+  .catch(error => {
+    response.status(500).json({ error });
+  });
+});
+
+app.delete('/api/v1/users/:id', (request, response) => {
+  const { id } = request.params;
+
+  database('users').where({ id }).del()
+    .then(user => {
+      if (user) {
+        return response.status(202).json(`User ${id} was deleted from database`);
+      } else return response.status(422).json({ error: 'Not Found' });
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+app.delete('/api/v1/favorites/:id', (request, response) => {
+  const { id } = request.params;
+
+  database('favorites').where({ id }).del()
+    .then(favorite => {
+      if (favorite) {
+        return response.status(202).json(`Favorite ${id} was deleted from database`);
+      } else return response.status(422).json({ error: 'Not Found' });
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`User Data API is running on ${app.get('port')}.`);
+});
+
+module.exports = app;
