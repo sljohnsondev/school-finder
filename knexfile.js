0e44a5eff8f8a1525bb4a47f0a2d6a372674ab46
sljohnson32 <sljohnson32@gmail.com>
2017-11-05 11:21:48 -0700

    Add production config to knexfile

--- knexfile.js
+++ knexfile.js
@@ -22,6 +22,13 @@ module.exports = {
       directory:'./db/seeds/test'
     },
     useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   }
-
 };
