540a9f9652568e5cce514f245defacb8f383440c
sljohnson32 <sljohnson32@gmail.com>
2017-11-05 12:26:38 -0700

    Fix merge issues

--- src/components/FavoriteButton/index.js
+++ src/components/FavoriteButton/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import './FavoriteButton.css';
-import React from 'react';
 import heart from '../../assets/heart.svg'
+import './FavoriteButton.css';
 
 const favoriteButton = (props) => {
   return (
