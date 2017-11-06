b8abf8f4ec086911dc992caa88706ad1a41526f7
Dan Alvarez <danalvarez5280@gmail.com>
2017-11-05 18:27:37 -0700

    stated fe testing

--- test/reducers/reducers.test.js
+++ test/reducers/reducers.test.js
@@ -1,9 +1,39 @@
 // import app from '../../src/reducers/app-reducer';
 import app from '../../src/reducers/app-reducer';
+import filters from '../../src/reducers/filters-reducer.js'
 
 describe('todos reducer', () => {
 
+  const mockInfo = {
+    username: 'Danman',
+    email: 'dan@dan.com',
+    street_address: '12345 St.',
+    oath_id: '1'
+  }
+
+  const mockAction1 = {
+    type: 'SIGN_IN',
+    data: mockInfo
+  };
+
+  const mockAction2 = {
+    type: 'FOOD_SEARCH',
+    data: mockInfo
+  };
+
+  const state = {};
+
   it('should return the initial state', () => {
-    expect( (undefined, {}) ).toEqual([])
+    expect( (undefined, {}) ).toEqual({})
   });
+
+  it('should not do anything when other reducers fire', ()=> {
+    expect(app(state, mockAction2.type)).toEqual({})
+  });
+
+  it('should user information', () => {
+    expect(app(mockAction1.data, mockAction1.type)).toEqual(mockInfo)
+  });
+
+
 });
