ff7d4d75c61dc7b5d7b598c0dee7b64062eea32a
Dan Alvarez <danalvarez5280@gmail.com>
2017-11-07 16:48:51 -0700

    add linted all containers

--- react-ui/src/containers/Filters-container.js
+++ react-ui/src/containers/Filters-container.js
@@ -1,44 +1,44 @@
 import { connect } from 'react-redux';
 import { updateAddress, patchUserAddress, setSchools, clearSchools, setHomeAddress, setDirections, clearDirections, toggleInfoWindow, activeSearchToggle } from '../actions';
 
-const mapStateToProps = (state) => {
-  return {
+const mapStateToProps = state => (
+  {
     schoolResults: state.FilterResults,
     activeSearch: state.FilterResults.activeSearch,
-    CurrentUser: state.CurrentUser
+    CurrentUser: state.CurrentUser,
   }
-}
+);
 
-const mapDispatchToProps = (dispatch) => {
-  return {
+const mapDispatchToProps = dispatch => (
+  {
     setSchools: (schoolResults, commuteTime, commuteDistance) => {
-      dispatch(setSchools(schoolResults, commuteTime, commuteDistance))
+      dispatch(setSchools(schoolResults, commuteTime, commuteDistance));
     },
     clearSchools: () => {
-      dispatch(clearSchools())
+      dispatch(clearSchools());
     },
     setHomeAddress: (homeAddress) => {
-      dispatch(setHomeAddress(homeAddress))
+      dispatch(setHomeAddress(homeAddress));
     },
     setDirections: (directions) => {
-      dispatch(setDirections(directions))
+      dispatch(setDirections(directions));
     },
     clearDirections: () => {
-      dispatch(clearDirections())
+      dispatch(clearDirections());
     },
     toggleInfoWindow: (marker) => {
-      dispatch(toggleInfoWindow(marker))
+      dispatch(toggleInfoWindow(marker));
     },
     activeSearchToggle: () => {
-      dispatch(activeSearchToggle())
+      dispatch(activeSearchToggle());
     },
-    patchUser: (address, userId, updatedUser) => {
-      dispatch(patchUserAddress(address, userId, updatedUser))
+    patchUser: (address, userId) => {
+      dispatch(patchUserAddress(address, userId));
     },
     updateAddress: (address) => {
-      dispatch(updateAddress(address))
-    }
+      dispatch(updateAddress(address));
+    },
   }
-}
+);
 
 export default connect(mapStateToProps, mapDispatchToProps);
--- react-ui/src/containers/Profile-container.js
+++ react-ui/src/containers/Profile-container.js
@@ -2,19 +2,19 @@ import { connect } from 'react-redux';
 import Profile from '../components/UserProfile/index';
 import { getUser } from '../actions';
 
-const mapStateToProps = (state) => {
-  return {
+const mapStateToProps = state => (
+  {
     user: state.CurrentUser,
-    favorites: state.Favorites
+    favorites: state.Favorites,
   }
-}
+);
 
-const mapDispatchToProps = (dispatch) => {
-  return {
+const mapDispatchToProps = dispatch => (
+  {
     getUser: (id, userInfo) => {
-      dispatch(getUser(id, userInfo))
-    }
+      dispatch(getUser(id, userInfo));
+    },
   }
-}
+);
 
 export default connect(mapStateToProps, mapDispatchToProps)(Profile);
