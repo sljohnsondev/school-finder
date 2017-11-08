dfee73dead98bffbf70e29dc47ad8fb9a3b39e30
Jonathan Beckman <jbecks.media@gmail.com>
2017-11-07 19:36:32 -0700

    add chart functionality to compare component

--- react-ui/src/components/Chart/index.js
+++ react-ui/src/components/Chart/index.js
@@ -10,24 +10,34 @@ class Chart extends Component {
 
     console.log('chart data 1', chart1data);
     console.log('chart data 2', chart2data);
+    let chartData = {};
+
+    if (chart1data && chart2data) {
+      const americanIndainAlaskanNative = chart1data.female_ai_an_count + chart1data.male_ai_an_count;
+      const asian = chart1data.female_asian_count + chart1data.male_asian_count;
+      const blackAfricanAmerican = chart1data.female_b_aa_count + chart1data.male_b_aa_count;
+      const hispanicLatino = chart1data.female_h_l_count + chart1data.male_h_l_count;
+      const multiRacial = chart1data.female_multi_racial_count + chart1data.male_multi_racial_count;
+      const nativeHawiaanOrPacificIslander = chart1data.female_nh_opi_count + chart1data.male_nh_opi_count;
+      const white = chart1data.female_white_count + chart1data.male_white_count;
+      const divideBy = americanIndainAlaskanNative + asian + blackAfricanAmerican + hispanicLatino + multiRacial + nativeHawiaanOrPacificIslander + white
+      
+      console.log('divdeby ', asian / divideBy);
+      
+      chartData = {
+        datasets: [{
+            data: [(americanIndainAlaskanNative / divideBy) * 100, (asian / divideBy) * 100, (blackAfricanAmerican / divideBy) * 100, (hispanicLatino / divideBy) * 100, (multiRacial / divideBy) * 100, (nativeHawiaanOrPacificIslander / divideBy) * 100, (white / divideBy) * 100],
+            backgroundColor: ['blue', 'green', 'purple', 'orange', 'yellow', 'cyan', 'coral']
+        }],
     
-    const data = {
-      datasets: [{
-          data: [25, 20, 30, 20, 5]
-      }],
-  
-      labels: [
-          'Red',
-          'Yellow',
-          'Blue',
-          'Orange',
-          'Purple'
-      ]
-  };
+        
+        labels: ['American Indain or Alaskan Native', 'Asian', 'Black African-American', 'Hispanic Latino', 'Multi Racial', 'Native Hawiaan Or Pacific Islander', 'White']
+      };
+    }
 
     return (
       <div className='chart-container'>
-        < Pie data={data} />
+        < Pie data={ chartData } />
       </div>
     );
   }
