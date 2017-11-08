e479c582a4f8cb0588b130a406f06216e2729dd2
Dan Alvarez <danalvarez5280@gmail.com>
2017-11-07 21:06:42 -0700

    add linter to UserProfile component

--- react-ui/src/components/UserProfile/index.js
+++ react-ui/src/components/UserProfile/index.js
@@ -5,51 +5,63 @@ import alias from '../../assets/user.svg';
 import './user-profile.css';
 
 export default class UserProfile extends Component {
-
   componentWillReceiveProps(nextProps) {
     const userInfo = {
       username: nextProps.name,
       email: nextProps.email,
-      oath_id: nextProps.userId
-    }
+      oath_id: nextProps.userId,
+    };
 
     if (nextProps.userId !== this.props.userId) {
-      this.props.getUser(nextProps.userId, userInfo)
+      this.props.getUser(nextProps.userId, userInfo);
     }
   }
 
   render() {
+    const {
+      name,
+      email,
+      photo,
+      tab,
+      toggleTab,
+      favorites,
+    } = this.props;
 
-    const { name, email, photo, tab, toggleTab, favorites } = this.props;
-    
-    const mappedFavorites = favorites.map( school => <Favorites school={ school } commuteInfo={ null } key={ school.school_id } /> );
+    const mappedFavorites = favorites.map(school =>
+      <Favorites school={school} commuteInfo={null} key={school.school_id} />);
 
-    let hideProfile = hideComponent(tab, 'profile');
+    const hideProfile = hideComponent(tab, 'profile');
 
     return (
 
-      <div className='profile'>
+      <div className="profile">
 
-        <button className={ hideProfile ? "slide-profile-btn hidden-profile" : "slide-profile-btn" } onClick={ () => toggleTabView(tab, toggleTab, 'profile') }>{ hideProfile ? '<' : '>' }</button>
+        <button
+          className={hideProfile ?
+          'slide-profile-btn hidden-profile' : 'slide-profile-btn'}
+          onClick={() => toggleTabView(tab, toggleTab, 'profile')}
+        >
+          {hideProfile ? '<' : '>'}
+        </button>
 
-        <div className={ hideProfile ? 'avatar show-avatar' : 'avatar hide-avatar'}>
-          <img src={ photo ? photo : alias } alt='avatar' className='avatar-photo' />
+        <div className={hideProfile ? 'avatar show-avatar' : 'avatar hide-avatar'}>
+          <img src={photo ? photo : alias} alt="avatar" className="avatar-photo" />
         </div>
 
-        <div className={ hideProfile ? 'profile-container hide-profile' : 'profile-container' }>
+        <div className={hideProfile ? 'profile-container hide-profile' : 'profile-container'}>
 
-          <div className='user-info'>
-            <h2 className='profile-header'>{ name }</h2>
+          <div className="user-info">
+            <h2 className="profile-header">{ name }</h2>
 
-            <img src={ photo ? photo : alias } alt='avatar' className='user-photo' />
+            <img src={photo ? photo : alias} alt="avatar" className="user-photo" />
 
             <h4 className="address">1771 17th St. Denver, CO. 80201 USA MothaLicka</h4>
-            <h4 className='email'>{ email }</h4>
+            <h4 className="email">{ email }</h4>
           </div>
 
           <div>
-            <h3 className='your-favorites'>Your Favorites</h3>
-            <div className='favorite-schools'>
+            <h3 className="your-favorites">Your Favorites</h3>
+            <div className="favorite-schools">
               { mappedFavorites }
             </div>
           </div>
@@ -57,6 +69,6 @@ export default class UserProfile extends Component {
         </div>
 
       </div>
-    )
-  };
-};
+    );
+  }
+}
