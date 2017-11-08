3e742270ab26e20f8f76ccd847941b7b60b3e1f6
Dan Alvarez <danalvarez5280@gmail.com>
2017-11-07 19:04:18 -0700

    add linter to Favorites component

--- react-ui/src/components/Favorites/index.js
+++ react-ui/src/components/Favorites/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import './favorites.css';
 import SchoolCard from '../../components/SchoolCard/SchoolCard';
 
-const Favorites = (props) => {
-
-  return (
-    <div className='favorites-container'>
-      <SchoolCard school={ props.school } />
+const Favorites = props => (
+  (
+    <div className="favorites-container">
+      <SchoolCard school={props.school} />
     </div>
   )
-};
+);
 
 export default Favorites;
