71dd64792c942360223db0540c704cb910a241a8
sljohnson32 <sljohnson32@gmail.com>
2017-11-08 11:01:05 -0700

    Update adv filter code in jsx

--- react-ui/src/components/AdvancedFilters/index.jsx
+++ react-ui/src/components/AdvancedFilters/index.jsx
@@ -1,16 +1,15 @@
-/* eslint-disable no-alert, class-methods-use-this */
 import React, { Component } from 'react';
 import './advanced-filters-style.css';
 
 export default class AdvancedFilters extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
-      hideAdvFilter: true,
-    };
+      hideAdvFilter: true
+    }
   }
 
-  toggleAdvancedFilter() {
+  toggleAdvancedFilter(tab) {
     if (this.state.hideAdvFilter === true) {
       this.setState({ hideAdvFilter: false });
     } else if (this.state.hideAdvFilter === false) {
@@ -19,218 +18,165 @@ export default class AdvancedFilters extends Component {
   }
 
   displayGradeFilters(props) {
-    const {
-      gradeLevel,
-      elaScore,
-      geometryScore,
-      scienceScore,
-      satScore,
-      handleChange,
-    } = props;
+
+    let { gradeLevel, elaScore, mathScore, scienceScore, satScore, handleChange} = props;
 
     if (gradeLevel === '1') {
-      return (
-
-        <p className="filter-item">
-          There are currently no additional metrics to share for Pre-K and ECE programs
-        </p>
-      );
+      return (<p className='filter-item' style={{ marginTop: '18px', padding: '0 12px' }}>There are currently no additional metrics to share for Pre-K and ECE programs.</p>)
     }
 
     if (gradeLevel === '2') {
       return (
         <div>
-          <article className="filter-item">
-            <h5>ELA (5th grade)</h5>
-            <input
-              id="elaScore"
-              className="slider"
-              type="range"
-              max="100"
-              value={elaScore}
-              onChange={e => handleChange(e)}
-            />
-            <p className="slider-data">{ elaScore }%</p>
-          </article>
-          <article className="filter-item">
+          <article className='filter-item'>
             <h5>Math (5th grade)</h5>
             <input
-              id="geometryScore"
-              className="slider"
+              id='mathScore'
+              className='slider'
               type="range"
               max="100"
-              value={geometryScore}
-              onChange={e => handleChange(e)}
+              value={ mathScore }
+              onChange={(e) => handleChange(e)}
             />
-            <p className="slider-data">{ geometryScore }%</p>
+            <p className='slider-data'>{ mathScore }%</p>
           </article>
-          <article className="filter-item">
+          <article className='filter-item'>
             <h5>Science (5th grade)</h5>
             <input
-              id="scienceScore"
-              className="slider"
+              id='scienceScore'
+              className='slider'
               type="range"
               max="100"
-              value={scienceScore}
-              onChange={e => handleChange(e)}
+              value={ scienceScore }
+              onChange={(e) => handleChange(e)}
             />
-            <p className="slider-data">{ scienceScore }%</p>
+            <p className='slider-data'>{ scienceScore }%</p>
           </article>
         </div>
-      );
+      )
     }
 
     if (gradeLevel === '3') {
       return (
         <div>
-          <article className="filter-item">
-            <h5>ELA (8th grade)</h5>
-            <input
-              id="elaScore"
-              className="slider"
-              type="range"
-              max="100"
-              value={elaScore}
-              onChange={e => handleChange(e)}
-            />
-            <p className="slider-data">{elaScore}%</p>
-          </article>
-          <article className="filter-item">
+          <article className='filter-item'>
             <h5>Math (8th grade)</h5>
             <input
-              id="geometryScore"
-              className="slider"
+              id='mathScore'
+              className='slider'
               type="range"
               max="100"
-              value={geometryScore}
-              onChange={e => handleChange(e)}
+              value={ mathScore }
+              onChange={(e) => handleChange(e)}
             />
-            <p className="slider-data">{geometryScore}%</p>
+            <p className='slider-data'>{ mathScore }%</p>
           </article>
-          <article className="filter-item">
+          <article className='filter-item'>
             <h5>Science (8th grade)</h5>
             <input
-              id="scienceScore"
-              className="slider"
+              id='scienceScore'
+              className='slider'
               type="range"
               max="100"
-              value={scienceScore}
-              onChange={e => handleChange(e)}
+              value={ scienceScore }
+              onChange={(e) => handleChange(e)}
             />
-            <p className="slider-data">{ scienceScore }%</p>
+            <p className='slider-data'>{ scienceScore }%</p>
           </article>
         </div>
-      );
+      )
     }
 
-    return (
-      <div>
-        <article className="filter-item">
-          <h5>ELA (9th grade)</h5>
-          <input
-            id="elaScore"
-            className="slider"
-            type="range"
-            max="100"
-            value={elaScore}
-            onChange={e => handleChange(e)}
-          />
-          <p className="slider-data">{ elaScore }%</p>
-        </article>
-        <article className="filter-item">
-          <h5>HS Geometry</h5>
-          <input
-            id="geometryScore"
-            className="slider"
-            type="range"
-            max="100"
-            value={geometryScore}
-            onChange={e => handleChange(e)}
-          />
-          <p className="slider-data">{ geometryScore }%</p>
-        </article>
-        <article className="filter-item">
-          <h5>HS Science</h5>
-          <input
-            id="scienceScore"
-            className="slider"
-            type="range"
-            max="100"
-            value={scienceScore}
-            onChange={e => handleChange(e)}
-          />
-          <p className="slider-data">{ scienceScore }%</p>
-        </article>
-        <article className="filter-item">
-          <h5>SAT</h5>
-          <input
-            id="satScore"
-            className="slider"
-            type="range"
-            max="100"
-            value={satScore}
-            onChange={e => handleChange(e)}
-          />
-          <p className="slider-data">{ satScore }%</p>
-        </article>
-      </div>
-    );
+    if (gradeLevel === '4') {
+      return (
+        <div>
+          <article className='filter-item'>
+            <h5>HS Geometry</h5>
+            <input
+              id='mathScore'
+              className='slider'
+              type="range"
+              max="100"
+              value={ mathScore }
+              onChange={(e) => handleChange(e)}
+            />
+            <p className='slider-data'>{ mathScore }%</p>
+          </article>
+          <article className='filter-item'>
+            <h5>HS Science</h5>
+            <input
+              id='scienceScore'
+              className='slider'
+              type="range"
+              max="100"
+              value={ scienceScore }
+              onChange={(e) => handleChange(e)}
+            />
+            <p className='slider-data'>{ scienceScore }%</p>
+          </article>
+          <article className='filter-item' style={{ marginTop: '30px' }}>
+            <h5 style={{ fontSize: '15px' }}>SAT (mean score)</h5>
+            <input
+              id='satScore'
+              className='slider'
+              type="range"
+              max="1250"
+              value={ satScore }
+              onChange={(e) => handleChange(e)}
+            />
+            <p className='slider-data'>{ satScore }</p>
+          </article>
+        </div>
+      )
+    }
   }
 
   render() {
-    const {
-      tab,
-      gradeLevel,
-      viewFilters,
-      studentTeacherRatio,
-      handleChange,
-    } = this.props;
-    const hidden = tab !== 'filters';
-    const hideAdvFilter = this.state.hideAdvFilter || !viewFilters;
+
+    let { tab, gradeLevel, viewFilters, studentTeacherRatio, handleChange } = this.props;
+    let hidden = tab !== 'filters' ? true : false;
+    let hideAdvFilter  = this.state.hideAdvFilter || !viewFilters ? true : false;
 
     if (tab !== 'filters' && !this.state.hideAdvFilter) {
-      this.setState({ hideAdvFilter: true });
+      this.setState({ hideAdvFilter: true })
     }
 
     return (
 
-      <div className={hidden ? 'adv-filters-hidden' : 'adv-filters'}>
-        <button
-          className={hideAdvFilter ? 'slide-adv-filter-btn hidden-adv-filter' :
-                                      'slide-adv-filter-btn'}
-          onClick={() => this.toggleAdvancedFilter(tab)}
-          disabled={!viewFilters || gradeLevel === ''}
-        >
-          { hideAdvFilter ? '>' : '<' }<p>More Filters</p>
-        </button>
-        { tab === 'filters' ?
-          <div className={hideAdvFilter ? 'adv-filter-container hidden-adv-filter' : 'adv-filter-container'}>
-            <form className="filter-fields">
-              <article className="filter-item">
-                <h5>Student/Teacher Ratio</h5>
-                <input
-                  id="studentTeacherRatio"
-                  className="slider"
-                  type="range"
-                  max="200"
-                  value={studentTeacherRatio}
-                  onChange={e => handleChange(e)}
-                />
-                <p className="slider-data">{studentTeacherRatio}</p>
-              </article>
-              <h5 className="filter-header" style={{ textDecoration: 'underline', fontSize: '16px' }}>CMAS Metrics (2016)</h5>
-              <p>% of students who met/exceeded expectations</p>
-              { gradeLevel ?
+      <div className={ hidden ? 'adv-filters-hidden' : 'adv-filters'}>
+        <button className={ hideAdvFilter ? "slide-adv-filter-btn hidden-adv-filter" :
+                                            "slide-adv-filter-btn"}
+                onClick={ () => this.toggleAdvancedFilter(tab) }
+                disabled={ !viewFilters || gradeLevel == '' }
+        >{ hideAdvFilter ? '>' : '<' }<p>More Filters</p></button>
+        { tab == 'filters' ?
+            <div className={ hideAdvFilter ? 'adv-filter-container hidden-adv-filter' : 'adv-filter-container'}>
+              <form className='filter-fields'>
+                <article className='filter-item'>
+                  <h5 style={{ fontSize: '15px' }}>Student/Teacher Ratio</h5>
+                  <input
+                    id='studentTeacherRatio'
+                    className='slider'
+                    type="range"
+                    max="200"
+                    value={ studentTeacherRatio }
+                    onChange={(e) => handleChange(e)}
+                  />
+                  <p className='slider-data'>{studentTeacherRatio} students/teacher</p>
+                </article>
+                <h5 className='filter-header' style={{ marginTop: '25px', textDecoration: 'underline', fontSize: '18px' }}>CMAS Metrics (2016)</h5>
+                <p style={{ fontSize: '14px', marginTop: '5px', padding: '0 10px' }}>% of students who met or exceeded expectations</p>
+                { gradeLevel ?
                   this.displayGradeFilters(this.props)
                   :
                   <p>Please select a grade level to view additional filters</p>
                 }
-            </form>
-          </div>
+              </form>
+            </div>
             :
-          <div style={{ display: 'none' }} />
+            <div style={{ display: "none" }} />
         }
       </div>
-    );
+    )
   }
 }
