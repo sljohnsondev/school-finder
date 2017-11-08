083056af3fef4aa79946d2f993da9a0f6fae90b2
sljohnson32 <sljohnson32@gmail.com>
2017-11-07 20:41:30 -0700

    Move favorite outside of click

--- react-ui/src/components/SearchResults/index.js
+++ react-ui/src/components/SearchResults/index.js
@@ -16,15 +16,17 @@ export default class SearchResults extends Component {
     let { refNum, schoolData, userId } = this.props;
 
     return (
-      <div className={this.props.selectedSchool === name ? 'school-container selected' : 'school-container'} onClick={() => this.toggleSchoolSelect()}>
-        <h3 className='results-fields'>{`${refNum + 1}. ${name}`}</h3>
-        <p className='results-fields'>{address}</p>
-        <a href={website} className='school-url results-fields' target='_blank'>School Website</a>
-        <section className='commute-info results-fields'>
-          <h4 className='results-fields'>Commute Time: <span className='commute-info'>{commute.time.text}</span></h4>
-          <h4 className='results-fields'>Commute Distance: <span className='commute-info'>{commute.distance.text}</span></h4>
-        </section>
-      <FavoriteButton schoolInfo={ schoolData } commuteInfo={ commute } userId={ userId } />
+      <div>
+        <div className={this.props.selectedSchool === name ? 'school-container selected' : 'school-container'} onClick={() => this.toggleSchoolSelect()}>
+          <h3 className='results-fields'>{`${refNum + 1}. ${name}`}</h3>
+          <p className='results-fields'>{address}</p>
+          <a href={website} className='school-url results-fields' target='_blank'>School Website</a>
+          <section className='commute-info results-fields'>
+            <h4 className='results-fields'>Commute Time: <span className='commute-info'>{commute.time.text}</span></h4>
+            <h4 className='results-fields'>Commute Distance: <span className='commute-info'>{commute.distance.text}</span></h4>
+          </section>
+        </div>
+        <FavoriteButton schoolInfo={ schoolData } commuteInfo={ commute } userId={ userId } />
       </div>
     )
   }
