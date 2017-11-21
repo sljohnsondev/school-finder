ba8deaa28916fc3f0121b9e65498c7ad527c56d9
sljohnson32 <sljohnson32@gmail.com>
2017-11-21 13:23:30 -0700

    Add select component to Compare feature

--- react-ui/src/components/Compare/index.jsx
+++ react-ui/src/components/Compare/index.jsx
@@ -3,9 +3,29 @@ import React, { Component } from 'react';
 import { toggleTabView, hideComponent } from '../Helpers/tabControls';
 import filterContainer from '../../containers/Filters-container';
 import Chart from '../Chart';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 import './compare-style.css';
 
+const options = [
+  { value: 'school_population', label: 'School Population' },
+  { value: 'graduation_ge', label: 'Graduation by Gender/Ethnicity' },
+  { value: 'graduation_ipst', label: 'Graduation by Instructional Program Service Type' }
+]
+
 class Compare extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      dropDown: 'school_population'
+    }
+    this.logChange = this.logChange.bind(this)
+  }
+  logChange(val) {
+    console.log('Selected: ', val.value);
+    this.setState({ dropDown: val.value })
+  }
 
   render() {
     const { tab, toggleTab, comparedSchools, favorites } = this.props;
@@ -34,8 +54,17 @@ class Compare extends Component {
           { buttonText }
         </button>
         <div className={ hideCompare ? 'compare-container hidden-compare' : 'compare-container'}>
-          <Chart schools={ comparedSchools[0] } favorites={ favorites } />
-          <Chart schools={ comparedSchools[1] } favorites={ favorites } />
+          <section className='school-metrics-container'>
+            <Select
+              name="school-metric-dropdown"
+              value={this.state.dropDown}
+              options={options}
+              onChange={this.logChange}
+              closeOnSelect={true}
+            />
+            <Chart schools={ comparedSchools[0] } favorites={ favorites } />
+            <Chart schools={ comparedSchools[1] } favorites={ favorites } />
+          </section>
         </div>
       </div>
     );
